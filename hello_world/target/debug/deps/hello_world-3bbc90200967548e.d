5d19cb52b497cbd6c1f7e05ee8dca51e0206776d
Hakan Yildiz <yildizhakan88@gmail.com>
2023-09-06 11:00:22 +0000

    Hello World

---
+++ hello_world/target/debug/deps/hello_world-3bbc90200967548e.d
@@ -0,0 +1,5 @@
+/workspace/Rust_Fundamentals/hello_world/target/debug/deps/hello_world-3bbc90200967548e: src/main.rs
+
+/workspace/Rust_Fundamentals/hello_world/target/debug/deps/hello_world-3bbc90200967548e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/workspace/Rust_Fundamentals/hello_world/target/debug/hello_world: /workspace/Rust_Fundamentals/hello_world/src/main.rs
