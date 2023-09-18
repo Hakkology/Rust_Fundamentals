0f31d59d5a05bb1f6d7326b6b42b7d57fac6573c
Hakan Yildiz <yildizhakan88@gmail.com>
2023-09-18 13:33:15 +0000

    Functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,27 @@
 fn main() {
-    println!("Hello, world!");
 
-    another_function();
+    print_hello();
+    print_number_request(25);
+    print_labeled_measurement(4, 'm');
+    print_goodbye();
 }
 
-fn another_function() {
+fn print_hello(){
+    println!("Hello, world!");
+}
+
+fn print_goodbye() {
     println!("Goodbye world!.");
+}
+
+fn print_number_request(x: i32) {
+    println!("The value of x is: {double_the_number(x)}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn double_the_number(x: i32) -> i32 {
+    x = x * 2;
 }
