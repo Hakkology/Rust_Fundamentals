b7c33e7a84e2ae94c00f191ce0a94a2bf4812c02
Hakan Yildiz <yildizhakan88@gmail.com>
2023-09-20 05:43:32 +0000

    Temperature call done.

---
+++ temperature/target/debug/deps/temperature-3aa711c827de1f16.d
@@ -0,0 +1,5 @@
+/workspace/Rust_Fundamentals/temperature/target/debug/deps/temperature-3aa711c827de1f16: src/main.rs
+
+/workspace/Rust_Fundamentals/temperature/target/debug/deps/temperature-3aa711c827de1f16.d: src/main.rs
+
+src/main.rs:
---
+++ temperature/target/debug/temperature.d
@@ -0,0 +1,1 @@
+/workspace/Rust_Fundamentals/temperature/target/debug/temperature: /workspace/Rust_Fundamentals/temperature/src/main.rs
