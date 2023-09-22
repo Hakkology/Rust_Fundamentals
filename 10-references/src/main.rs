e00adcaedc8d41c0b1a4a6c9e7e2d5af3827b3ef
Hakan Yildiz <yildizhakan88@gmail.com>
2023-09-22 08:11:35 +0000

    Reference notes finalized.

--- 10-references/src/main.rs
+++ 10-references/src/main.rs
@@ -18,6 +18,9 @@ fn main() {
     // Two or more pointers access the same data at the same time.
     // At least one of the pointers is being used to write to the data.
     // There’s no mechanism being used to synchronize access to the data.
+
+    // At any given time, you can have either one mutable reference or any number of immutable references.
+    // References must always be valid.
 }
 
 fn calculate_length(s: &String) -> usize {
