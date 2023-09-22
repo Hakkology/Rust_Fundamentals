dd3fce57f18eee732848ab1d938ebc86f30ed2d4
Hakan Yildiz <yildizhakan88@gmail.com>
2023-09-22 08:10:42 +0000

    References and data race.

---
+++ 10-references/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    // There can only be one mutable reference, two would fail.
+
+    // let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+
+    // println!("{}, {}", r1, r2);
+
+    // data race conditions:
+    // Two or more pointers access the same data at the same time.
+    // At least one of the pointers is being used to write to the data.
+    // There’s no mechanism being used to synchronize access to the data.
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
